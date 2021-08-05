88472f9034ff930c10ed4f0340d2de9a7286a1ee
wnsrb003 <wnsrb003@naver.com>
2021-08-05 18:02:19 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { User, Domain } = require('../models');
+const { isLoggedIn } = require('./middlewares');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { id: req.user && req.user.id || null },
+      include: { model: Domain },
+    });
+    res.render('login', {
+      user,
+      domains: user && user.Domains,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post('/domain', isLoggedIn, async (req, res, next) => {
+  try {
+    await Domain.create({
+      UserId: req.user.id,
+      host: req.body.host,
+      type: req.body.type,
+      clientSecret: uuidv4(),
+    });
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
